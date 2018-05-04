18a47aa3ad454519303d2cd64621daa45cd7a187
jcapasix <jcapasix@everis.com>
2018-05-04 11:47:26 -0500

    add watch target

---
+++ TigoWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TigoWatch Extension
+//
+//  Created by Jordan Capa on 4/05/18.
+//  Copyright © 2018 Jordan Capa. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
