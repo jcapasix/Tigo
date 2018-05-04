78a99038de633bbb45bc0ec19b83b78145e7581a
jcapasix <jcapasix@everis.com>
2018-05-04 15:18:47 -0500

    se configuro WCSession para poder mandar mensajes entre el iPhone y el apple watch

--- Tigo/ViewController.swift
+++ Tigo/ViewController.swift
@@ -7,21 +7,85 @@
 //
 
 import UIKit
-import Firebase
+import WatchConnectivity
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    var lastMessage: CFAbsoluteTime = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        FirebaseApp.configure()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (WCSession.isSupported()) {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func tapButtonPressed(_ sender: Any) {
+        sendWatchMessage()
+    }
+    
+    func sendWatchMessage() {
+        let currentTime = CFAbsoluteTimeGetCurrent()
+        
+        // if less than half a second has passed, bail out
+        if lastMessage + 0.5 > currentTime {
+            return
+        }
+        
+        // send a message to the watch if it's reachable
+        if (WCSession.default.isReachable) {
+            // this is a meaningless message, but it's enough for our purposes
+            self.view.backgroundColor = UIColor.blue
+            let message = ["Message": "Tap from Phone"]
+            WCSession.default.sendMessage(message, replyHandler: nil)
+        }
+        
+        // update our rate limiting property
+        lastMessage = CFAbsoluteTimeGetCurrent()
+    }
+}
 
-
+extension ViewController: WCSessionDelegate{
+    
+    
+    // MARK: - WCSessionDelegate
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("function:\(#function)")
+        
+    }
+    
+    func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
+        print("function:\(#function)")
+        self.view.backgroundColor = UIColor.red
+    }
+    
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        print("function:\(#function)")
+        
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        print("function:\(#function)")
+        
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        DispatchQueue.main.async {
+            print("message: XXX: \(message)")
+            self.view.backgroundColor = UIColor.red
+        }
+    }
+    
 }
 
--- TigoWatch Extension/InterfaceController.swift
+++ TigoWatch Extension/InterfaceController.swift
@@ -8,24 +8,74 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
 class InterfaceController: WKInterfaceController {
-
+    
+    var lastMessage: CFAbsoluteTime = 0
+    var count = 0
+    let session = WCSession.default
+    
+    @IBOutlet var groupColor: WKInterfaceGroup!
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        if WCSession.isSupported() {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    @IBAction func TapButtonPressed() {
+        self.sendWatchMessage()
+        
+    }
+    
+    func sendWatchMessage() {
+        if WCSession.isSupported() {
+            let msg = ["Count" : "\(count)"]
+            
+            if session.isReachable {
+                self.groupColor.setBackgroundColor(UIColor.red)
+                session.sendMessage(msg, replyHandler: nil, errorHandler: { (error) -> Void in
+                    print("Error handler: \(error)")
+                })
+                count += 1
+            }
+        }
+    }
+    
+}
 
+extension InterfaceController: WCSessionDelegate{
+    
+    
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("function:\(#function)")
+    }
+    
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        print("function:\(#function)")
+        print("Message: \(message)")
+        self.groupColor.setBackgroundColor(UIColor.blue)
+        WKInterfaceDevice().play(.click)
+    }
+    
+    
 }
